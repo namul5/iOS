6a73e8c0ae6b3ca4d9797c89435e5ff0b736fb89
Luke Lim <solnamul@gmail.com>
2017-06-13 00:40:57 +0900

    HelloWorld is my first ios app.

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Luke Lim on 2017. 6. 12..
+//  Copyright © 2017년 Luke Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lblHello: UILabel!
+    @IBOutlet weak var txtName: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func btnSend(_ sender: UIButton) {
+        lblHello.text = "Hello, " + txtName.text!
+    }
+
+}
+
